a3d5c61653119ffb5bdf10556d24061f15e2f03c
Darshan Viradiya <drshnptl137@gmail.com>
2024-01-16 20:39:45 +0530

    Add TotalStudentStateTable component

---
+++ src/views/admin/attendancemanagement/studentattendancepanel/attendancegraphs/components/TotalStudentStateTable.tsx
@@ -0,0 +1,45 @@
+import Card from "../../../../../../components/card";
+import {
+  Pagenation,
+  TBody,
+  THeade,
+  Table,
+  Td,
+  Th,
+  Tr,
+} from "../../../../../../components/table/Table";
+
+function TotalStudentStateTable() {
+
+  return (
+    <Card
+      extra={
+        "w-full h-fit sm:overflow-auto rounded-xl bg-white dark:bg-navy-800 px-5 my-5 border-0"
+      }
+    >
+      <Table tableExtras="w-full">
+        <THeade>
+          <Tr extras="border-b-2">
+            <Th>Total Student</Th>
+            <Th>Total Present</Th>
+            <Th>Total Absent</Th>
+            <Th>Total Leave</Th>
+            <Th>Not Mark</Th>
+          </Tr>
+        </THeade>
+
+        <TBody>
+         <Tr>
+            <Td>2333</Td>
+            <Td>2000</Td>
+            <Td>300</Td>
+            <Td>33</Td>
+            <Td>0</Td>
+         </Tr>
+        </TBody>
+      </Table>
+    </Card>
+  );
+}
+
+export default TotalStudentStateTable;
