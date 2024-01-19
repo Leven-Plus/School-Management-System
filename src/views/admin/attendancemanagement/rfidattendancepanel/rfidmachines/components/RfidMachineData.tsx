eadd8510fe8bc360854925f2273e2305c45bfd51
Darshan Viradiya <drshnptl137@gmail.com>
2024-01-19 10:02:32 +0530

    Add RfidMachineData component for displaying RFID machine data

---
+++ src/views/admin/attendancemanagement/rfidattendancepanel/rfidmachines/components/RfidMachineData.tsx
@@ -0,0 +1,143 @@
+import { useEffect, useState } from "react";
+import CardMenu from "../../../../../../components/card/CardMenu";
+import Card from "../../../../../../components/card";
+import {
+  TBody,
+  THeade,
+  Table,
+  Td,
+  Th,
+  Tr,
+} from "../../../../../../components/table/Table";
+import Spinner from "../../../../../../components/spinner/spinner";
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+function RfidMachineData(props: { tableData: any; tableName?: string }) {
+  const { tableData: tableDataMain, tableName } = props;
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [perPagex, setPerPage] = useState(
+    Math.round(getWindowDimensions()?.height - 390 / 64)
+  );
+  const [totalPagex, setTotalPage] = useState(0);
+
+  const [loadOrders, setLoadOrders] = useState(false);
+
+  useEffect(() => {
+    setTotalPage(Math.ceil(tableDataMain?.length / perPagex));
+
+    setLoadOrders(true);
+
+    setData([]);
+
+    setTimeout(() => {
+      setData(tableDataMain.slice(perPagex * (page - 1), perPagex * page));
+      setLoadOrders(false);
+    }, 100);
+  }, [page]);
+
+  useEffect(() => {
+    setPerPage(Math.round((getWindowDimensions()?.height - 390) / 64));
+
+    window.addEventListener("resize", () =>
+      setPerPage(Math.round((getWindowDimensions()?.height - 390) / 64))
+    );
+
+    return () =>
+      window.removeEventListener("resize", () =>
+        setPerPage(Math.round((getWindowDimensions()?.height - 390) / 64))
+      );
+  }, []);
+
+  useEffect(() => {
+    setTotalPage(Math.ceil(tableDataMain?.length / perPagex));
+
+    setLoadOrders(true);
+
+    setData([]);
+
+    setTimeout(() => {
+      setData(tableDataMain.slice(perPagex * (page - 1), perPagex * page));
+      setLoadOrders(false);
+    }, 100);
+  }, [perPagex]);
+
+  console.log(perPagex);
+
+  return (
+    <Card
+      extra={
+        "w-full h-max sm:overflow-auto rounded-xl bg-white dark:bg-navy-800 p-5 mb-5 border-0 col-span-3"
+      }
+    >
+      <header className="relative flex items-center justify-between">
+        <h2 className="text-2xl font-bold dark:text-white">
+          {tableName ? tableName : "Table"}
+        </h2>
+        <CardMenu />
+      </header>
+
+      <Table tableExtras="w-[100vw]">
+        <THeade>
+          <Tr>
+            <Th>S No.</Th>
+            <Th>Machine ID</Th>
+            <Th>Student Name / Employee Name</Th>
+            <Th>RFID Number</Th>
+            <Th>ID</Th>
+            <Th>Class[Section] / Department[Designation]</Th>
+            <Th>Entry</Th>
+            <Th>Exit</Th>
+          </Tr>
+        </THeade>
+
+        <TBody>
+          {tableDataMain.map((r: any, r_i: number) => {
+              if (r_i % 2 === 0) {
+                return (
+                  <Tr extras="border-b-2">
+                    <Td extras="pl-3">{r_i + 1}</Td>
+                    <Td>SFTIDRFID{r_i + 1}</Td>
+                    <Td>Yougesh Maurya</Td>
+                    <Td>RFID22332432</Td>
+                    <Td>3423</Td>
+                    <Td>Class 7-[A]</Td>
+                    <Td>{new Date().toString()}</Td>
+                    <Td>{new Date().toString()}</Td>
+                  </Tr>
+                );
+              }else{
+                return (
+                  <Tr extras="border-b-2">
+                    <Td extras="pl-3">{r_i + 1}</Td>
+                    <Td>EMPRFID{r_i + 1}</Td>
+                    <Td>Jatin Patel</Td>
+                    <Td>RFID26632432</Td>
+                    <Td>323121</Td>
+                    <Td>Teaching[Teacher]</Td>
+                    <Td>{new Date().toString()}</Td>
+                    <Td>{new Date().toString()}</Td>
+                  </Tr>
+                );
+              }
+          })}
+        </TBody>
+      </Table>
+
+      {loadOrders && (
+        <div className="h-[500px] w-full flex">
+          <Spinner />
+        </div>
+      )}
+    </Card>
+  );
+}
+
+export default RfidMachineData;
