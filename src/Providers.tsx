ff8f38e99cd510b15392c78f59bb3bfead33034c
Darshan Viradiya <drshnptl137@gmail.com>
2024-01-24 13:54:47 +0530

    Add CookiesProvider to Providers component

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-
 import { Provider } from "react-redux";
 import { ChakraProvider } from "@chakra-ui/react";
 import store from "./app/store";
+import { CookiesProvider } from "react-cookie";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
-      <ChakraProvider>
-        {children}
-      </ChakraProvider>
+      <CookiesProvider>
+        <ChakraProvider>{children}</ChakraProvider>
+      </CookiesProvider>
     </Provider>
   );
 }
